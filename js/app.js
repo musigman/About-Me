e3e96773a320925780bdff014fc5d3c6dee1ca67
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-08-05 08:58:50 -0700

    Add new files

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// 'use strict';
+
+// var userName = prompt('Hello! What is your name?');
+// alert('Welcome to my page ' + userName);
+
+// var doYouLikeCheese = prompt('Do you like cheese?');
+// // console.log('Does the user like cheese?' + doYouLikeCheese);
+// if (doYouLikeCheese.toLowerCase() === 'yes') {
+//   alert('Awesome! I like cheese too!');
+// } else {
+//   alert('Bummer. You might be lactose intollerant');
+// }
+
+// var isSkyBlue = prompt('Is the sky blue?');
+// // console.log('Is the sky blue?' + isSkyBlue);
+// if (isSkyBlue.toLowerCase() === 'yes') {
+//   alert('Yeah I think so too!');
+// } else {
+//   alert('Really...Is it gray?');
+// }
+
+// var mustardOnHotDog = prompt('Do you like mustard on hot dogs?');
+// // console.log('Does the user like mustard on hotdogs?' + mustardOnHotDog);
+// if (mustardOnHotDog.toLowerCase() === 'yes') {
+//   alert('Sweet! So do I!');
+// } else {
+//   alert('Hmm..maybe try catchup');
+// }
+
+// var isMonthOfAugust = prompt('Is it the month of August');
+// // console.log('It is the month of August?' + isMonthOfAugust);
+// if (isMonthOfAugust.toLowerCase() === 'yes') {
+//   alert('It\'s summer time!');
+// } else {
+//   alert('Winter must be on the way');
+// }
+
+// var isCodingFun = prompt('Is coding fun?');
+// // console.log('Is coding fun?' + isCodingFun);
+// if (isCodingFun.toLowerCase() === 'yes') {
+//   alert('Hell yeah it is!');
+// } else {
+//   alert('Get out of here!!');
+// }
+
+// alert('Once again...Welcome to my page ' + userName + '. I hope you can learn more about me here!');
+
